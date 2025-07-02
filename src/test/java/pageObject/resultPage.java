3558a269b3bd4d3558e385c456dbb188082ad3b8
2408457 <2408457@LTIN450058.cts.com>
2025-07-03 00:10:46 +0530

    removing sleep and adding wait

--- src/test/java/pageObject/resultPage.java
+++ src/test/java/pageObject/resultPage.java
@@ -10,50 +10,66 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class resultPage extends basePage{
+	
 	List<WebElement> ans= new ArrayList<>();
+	
 	public resultPage(WebDriver driver) {
 		super(driver);
 	}
+	
 	@FindBy(xpath="//*[@class='left']//li")
-	List<WebElement> ls;
+	List<WebElement> resultList;
+	
 	@FindBy(xpath="//span[text()='Read more info']")
-	WebElement rm;
+	WebElement readMoreInfo;
+	
 	@FindBy(xpath="//*[text()='Amenities']//following::div[@class='p-entity__item']/span")
 	List<WebElement> park;
+	
 	public boolean checkParking(WebElement w) {
 		w.findElement(By.xpath(".//a")).click();
 		Object[] wh = driver.getWindowHandles().toArray();
 		driver.switchTo().window((String)wh[1]);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("arguments[0].scrollIntoView();",rm);
-		rm.click();
-		for(WebElement i:park) {
-			String j = i.getText();
-			if(j.equals("Parking")) {
-				driver.close();
-				driver.switchTo().window((String)wh[0]);
-				return true;
+		try {
+			js.executeScript("arguments[0].scrollIntoView();",readMoreInfo);
+			readMoreInfo.click();
+			for(WebElement i:park) {
+				String j = i.getText();
+				if(j.equals("Parking")) {
+					driver.close();
+					driver.switchTo().window((String)wh[0]);
+					return true;
+				}
 			}
+			driver.close();
+			driver.switchTo().window((String)wh[0]);
+			return false;
 		}
-		driver.close();
-		driver.switchTo().window((String)wh[0]);
-		return false;
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
 		
 	}
 	public List<WebElement> takeList() {
-		for(WebElement e:ls) {
-			//System.out.print(e.findElement(By.tagName("h2")).getText()+" ");
-			String timing = e.findElement(By.xpath(".//div[@class='line-4']/span[2]/span")).getText();
-			Double rating = Double.parseDouble(e.findElement(By.xpath(".//div[@class='text-1']//span[@class='u-bold']")).getText());
-			if(timing.equals("Open 24x7") && rating>3.5 && checkParking(e)){
-				ans.add(e);
-				System.out.print(e.findElement(By.tagName("h2")).getText()+" ");
-				System.out.print(timing+" ");
-				System.out.println(rating+" ");
-				
+		for(WebElement e:resultList) {
+			try {
+				//System.out.print(e.findElement(By.tagName("h2")).getText()+" ");
+				String timing = e.findElement(By.xpath(".//div[@class='line-4']/span[2]/span")).getText();
+				Double rating = Double.parseDouble(e.findElement(By.xpath(".//div[@class='text-1']//span[@class='u-bold']")).getText());
+				if(timing.equals("Open 24x7") && rating>3.5 && checkParking(e)){
+					ans.add(e);
+					System.out.print(e.findElement(By.tagName("h2")).getText()+" ");
+					System.out.print(timing+" ");
+					System.out.println(rating+" ");
+				}
+			}
+			catch(Exception er) {
+				System.out.println(er.getMessage());
 			}
 		}
-		System.out.println(ls.size());
+		System.out.println(resultList.size());
 		return ans;
 	}
 	
--- src/test/java/testCases/T001_homePage.java
+++ src/test/java/testCases/T001_homePage.java
@@ -1,11 +1,15 @@
 package testCases;
 
+import java.time.Duration;
 import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import pageObject.homePage;
 import pageObject.resultPage;
@@ -19,6 +23,8 @@ public class T001_homePage {
 		hp.cityLoc();
 		JavascriptExecutor js=(JavascriptExecutor) driver;;
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight * 0.75);");
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@class='left']//li[11]")));
 		
 		resultPage rp = new resultPage(driver);
 		List<WebElement> ans = rp.takeList();
